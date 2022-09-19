41e979153b505a22518485bc18cf9d9e5bfd9f48
Dr. Brantley <dr.brantley@redhat.com>
2022-09-19 14:12:43 -0400

    Add initial Jenkins Job skeleton

---
+++ jenkins-job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "================================="
+echo "Running The Late Syft Jenkins Job"
+echo "================================="
+echo ""
+
+# Stuff
+# Goes
+# Here
+
+echo ""
+echo "====="
+echo "DONE."
+echo "====="
