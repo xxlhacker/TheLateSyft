f618e45296ddbe20acedd66acc0b909a0b674b7f
Dr. Brantley <dr.brantley@redhat.com>
2022-09-21 10:47:15 -0400

    Initial bash to invoke python script

--- jenkins-job.sh
+++ jenkins-job.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-echo "================================="
-echo "Running The Late Syft Jenkins Job"
-echo "================================="
-echo ""
 ./latesyftlogo.sh
 
 # Metadata ----------------------------------------------------------
@@ -22,35 +18,24 @@ echo ""
 echo "-- Environment --"
 env
 echo ""
-
-# Main Logic ---------------------------------------------------------
-# Stuff
-# Goes
-# Here
-echo "-- Scanning Quay.io Image --"
-echo $QUAY_IMAGE:$QUAY_TAG
+echo "-- Scanning Workstream --"
+echo $WORKSTREAM
+echo ""
+echo "-- Output Format --"
+echo $OUTPUT_FORMAT" "$OUTPUT_TEMPLATE
 echo ""
 
 # Make the artifacts results directory
 RESULTS_DIR=$WORKSPACE"/syft_results"
-echo "Making artifacts results directory "$RESULTS_DIR"..."
+echo "Making artifacts results directory "$RESULTS_DIR
 mkdir $RESULTS_DIR
 
-# Determine output template format
-if [ $SYFT_OUTPUT_FORMAT = "template" ]; then
-    SYFT_OUTPUT_TEMPLATE_FILE_FLAG="-t templates/"$SYFT_OUTPUT_TEMPLATE_FILE
-else
-    SYFT_OUTPUT_TEMPLATE_FILE_FLAG=""
-fi
-
-# Sanitize any bad strings for artifact filenames
-FORMATTED_QUAY_IMAGE=$(echo $QUAY_IMAGE | sed -e 's/[^A-Za-z0-9._-]/_/g')
-SYFT_OUTPUT_FILE=$RESULTS_DIR"/"$FORMATTED_QUAY_IMAGE"."$SYFT_OUTPUT_FORMAT
-
-# Perform Syft Analysis
-echo "Writing Syft results to file "$SYFT_OUTPUT_FILE"..."
-syft $QUAY_IMAGE:$QUAY_TAG -o $SYFT_OUTPUT_FORMAT=$SYFT_OUTPUT_FILE $SYFT_OUTPUT_TEMPLATE_FILE_FLAG
-echo ""
+# Run Python Syft script
+echo "Setting up Python environment..."
+pipenv shell
+pipenv install
+echo "Running syft-automation.py..."
+python syft-automation.py $WORKSTREAM
 
 echo ""
 ./fin.sh
