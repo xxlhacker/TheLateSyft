254c968c68b0b58dd22aa30f3a929895d10537a7
Casey Williams <caswilli@redhat.com>
2022-09-23 17:02:07 -0400

    Pushing Cleaning Functions

--- config.py
+++ config.py
@@ -3,5 +3,6 @@ import os
 SYFT_RESULTS_DIR = "results"
 WORKSTREAMS_DIR = "workstreams"
 TEMPLATES_DIR = "templates"
+CONTAINER_ENGINE = "docker"
 OSD_API_KEY = os.environ['OSD_API_KEY']
 PROD_OSD_API_URL = "https://api.crcp01ue1.o9m8.p1.openshiftapps.com:6443/"
